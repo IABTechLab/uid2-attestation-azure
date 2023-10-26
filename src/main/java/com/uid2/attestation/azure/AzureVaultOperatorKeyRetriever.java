d1e66eaa127058b9a7f9aa6f68d715bb06622cb2
Yi Shi （施毅） <yi.shi@thetradedesk.com>
2023-10-26 14:10:35 +0800

    add logic to handle azure operator key load

---
+++ src/main/java/com/uid2/attestation/azure/AzureVaultOperatorKeyRetriever.java
@@ -0,0 +1,37 @@
+package com.uid2.attestation.azure;
+
+import com.azure.identity.ManagedIdentityCredentialBuilder;
+import com.azure.security.keyvault.secrets.SecretClientBuilder;
+import com.uid2.enclave.IOperatorKeyRetriever;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AzureVaultOperatorKeyRetriever implements IOperatorKeyRetriever {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AzureVaultOperatorKeyRetriever.class);
+
+    private final String vaultName;
+    private final String secretName;
+
+
+    public AzureVaultOperatorKeyRetriever(String vaultName, String secretName) {
+        this.vaultName = vaultName;
+        this.secretName = secretName;
+    }
+
+    // ManagedIdentityCredential is used here.
+    @Override
+    public String retrieve() {
+        String vaultUrl = "https://" + this.vaultName + ".vault.azure.net";
+        LOGGER.info(String.format("Load OperatorKey secret (%s) from %s", this.secretName, vaultUrl));
+        // Use default ExponentialBackoff retry policy
+        var secretClient = new SecretClientBuilder()
+                .vaultUrl(vaultUrl)
+                .credential(new ManagedIdentityCredentialBuilder().build())
+                .buildClient();
+
+        var retrievedSecret = secretClient.getSecret(secretName);
+
+        LOGGER.info("OperatorKey secret is loaded.");
+        return retrievedSecret.getValue();
+    }
+}
